867da55ebf1f8b0ec8f04876a501f0c58f38da45
vaclav <vaclav.cz@gmail.com>
2017-01-18 12:29:12 +0100

    reduced code by creating for loop in onclick listener. Also removed unnecessary button array in onCreate() method.

--- app/src/main/java/com/vaclavhnizda/groupsecretsanta/MainActivity.java
+++ app/src/main/java/com/vaclavhnizda/groupsecretsanta/MainActivity.java
@@ -14,12 +14,15 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button[] myButtons = new Button[8];
+        Button myButton;
+
+        //itterate over all buttons
         for(int x = 0; x < 8; x++){
             int ResID = getResources().getIdentifier("button" + (x + 1),"id",getPackageName());
-            myButtons[x] = (Button)findViewById(ResID);
-            myButtons[x].setText((minCount + x) + " people");
-            myButtons[x].setOnClickListener(this);
+            myButton = (Button)findViewById(ResID);
+            myButton.setText((minCount + x) + " people");
+            myButton.setTag(minCount + x);
+            myButton.setOnClickListener(this);
         }
     }
 
@@ -27,33 +30,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
     @Override
     public void onClick(View view) {
 
-
-        switch(view.getId())
-        {
-            case R.id.button1:
-                ((Button)view).setText("Button 1 pressed!");
-                break;
-            case R.id.button2:
-                ((Button)view).setText("Button 2 pressed!");
-                break;
-            case R.id.button3:
-                ((Button)view).setText("Button 3 pressed!");
-                break;
-            case R.id.button4:
-                ((Button)view).setText("Button 4 pressed!");
-                break;
-            case R.id.button5:
-                ((Button)view).setText("Button 5 pressed!");
-                break;
-            case R.id.button6:
-                ((Button)view).setText("Button 6 pressed!");
-                break;
-            case R.id.button7:
-                ((Button)view).setText("Button 7 pressed!");
-                break;
-            case R.id.button8:
-                ((Button)view).setText("Button 8 pressed!");
-                break;
-        }
+        Button buttonClicked = (Button)view;
+        int personCount = (int)buttonClicked.getTag();
+        buttonClicked.setText("Button " + (personCount - minCount + 1) + " OK.");
     }
 }
