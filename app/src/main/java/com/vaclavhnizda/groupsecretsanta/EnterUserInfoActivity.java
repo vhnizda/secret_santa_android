b3b4e6117425830ff83f778601c158248c8790e7
vaclav <vaclav.cz@gmail.com>
2017-01-19 14:56:50 +0100

    Created new internal layout in second activity. It also creates the first row for entering user information. Once this works I will have it dynamically create all rows based on number of users requested.

--- app/src/main/java/com/vaclavhnizda/groupsecretsanta/EnterUserInfoActivity.java
+++ app/src/main/java/com/vaclavhnizda/groupsecretsanta/EnterUserInfoActivity.java
@@ -1,8 +1,13 @@
 package com.vaclavhnizda.groupsecretsanta;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class EnterUserInfoActivity extends AppCompatActivity {
@@ -19,6 +24,22 @@ public class EnterUserInfoActivity extends AppCompatActivity {
         textView.setText("Please fill out the information for all " + numberOfPeople + " people.");
 
         //TODO create a list of text fields for entering all the user data.
+        Context context = getApplicationContext();
+        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.user_list_layout);
+
+        LinearLayout rowLayout = new LinearLayout(context);
+        rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        TextView numbering = new TextView(context);
+        numbering.setText("1.");
+        rowLayout.addView(numbering);
+
+        EditText userInfo = new EditText(context);
+        userInfo.setHint("<enter name here>");
+        rowLayout.addView(userInfo);
+        //TODO figure out how to color text, currently all is white??
+
+        mainLayout.addView(rowLayout);
         //TODO add a button to move to next fields (and save data to database)
     }
 }
