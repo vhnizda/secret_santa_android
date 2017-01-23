07b80f3dce0122e4b17fe3da3bb56f68fe12ef3f
vaclav <vaclav.cz@gmail.com>
2017-01-23 14:51:50 +0100

    Text colors are now legible. Also was able to prevent the first text box from auto-selecting by default when the new activity is opened.

--- app/src/main/java/com/vaclavhnizda/groupsecretsanta/EnterUserInfoActivity.java
+++ app/src/main/java/com/vaclavhnizda/groupsecretsanta/EnterUserInfoActivity.java
@@ -2,12 +2,9 @@ package com.vaclavhnizda.groupsecretsanta;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,25 +22,25 @@ public class EnterUserInfoActivity extends AppCompatActivity {
         TextView textView = (TextView)findViewById(R.id.user_instructions);
         textView.setText("Please fill out the information for all " + numberOfPeople + " people.");
 
-        //TODO create a list of text fields for entering all the user data.
+        //Create a list of text fields for entering all the user data.
         Context context = getApplicationContext();
-        int colorBlack = Color.parseColor("#000000");
-
         LinearLayout mainLayout = (LinearLayout)findViewById(R.id.user_list_layout);
 
+
+        int rowNumber = 1;
         LinearLayout rowLayout = new LinearLayout(context);
         rowLayout.setOrientation(LinearLayout.HORIZONTAL);
-
         TextView numbering = new TextView(context);
-        numbering.setText("1.");
-        numbering.setTextColor(colorBlack);
+        numbering.setText(rowNumber + ". ");
+        numbering.setTextColor(Color.BLACK);
         rowLayout.addView(numbering);
 
         EditText userInfo = new EditText(context);
         userInfo.setHint("<enter name here>");
-        userInfo.setTextColor(colorBlack);
+        userInfo.setTextColor(Color.BLACK);
+        userInfo.setHintTextColor(Color.GRAY);
+
         rowLayout.addView(userInfo);
-        //TODO figure out how to color text, currently all is white??
 
         mainLayout.addView(rowLayout);
         //TODO add a button to move to next fields (and save data to database)
