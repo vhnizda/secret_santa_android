60777d5eb1197ebabaef150151aa59272f0020b8
vaclav <vaclav.cz@gmail.com>
2017-02-06 10:04:14 +0100

    removed all old code from initial buttons # of people selection.

--- app/src/main/java/com/vaclavhnizda/groupsecretsanta/MainActivity.java
+++ app/src/main/java/com/vaclavhnizda/groupsecretsanta/MainActivity.java
@@ -2,13 +2,11 @@ package com.vaclavhnizda.groupsecretsanta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.NumberPicker;
-import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,21 +33,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 //        TextView tv1 = (TextView)myNumPicker.getChildAt(0);
 //        tv1.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
 
-//        mainButton.setOnClickListener(this);
-
-        //iterate over all buttons - we have 8 of them pre-created
-        for(int x = 0; x < 8; x++){
-            //Used to get resource id based on "button" + int ID
-            int ResID = getResources().getIdentifier("button" + (x + 1),"id",getPackageName());
-            //Retrieve button from ID found
-            myButton = (Button)findViewById(ResID);
-            //Set the text of the button
-            myButton.setText((minCount + x) + " people");
-            //Set a tag for later retrieval
-            myButton.setTag(minCount + x);
-            //Create a listener link to the button click
-            //myButton.setOnClickListener(this);
-        }
     }
 
 
@@ -61,16 +44,4 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         intent.putExtra(PEOPLE_COUNT,numberOfPeople);
         startActivity(intent);
     }
-//    @Override
-//    public void onClick(View view) {
-//        //Only buttons are linked currently so it's safe to assume this is a button
-//        Button buttonClicked = (Button)view;
-//        //Get my custom int tag representing the number of people they want to include in secret santa
-//        int personCount = (int)buttonClicked.getTag();
-//
-//        //This is where the next activity is launched for the app.
-//        Intent intent = new Intent(this, EnterUserInfoActivity.class);
-//        intent.putExtra(PEOPLE_COUNT,personCount);
-//        startActivity(intent);
-//    }
 }
